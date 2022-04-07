8f21cda7a61c083b20b3eeb7b92017015f13dfeb
HugoLundberg <hugo.h.lundberg@gmail.com>
2022-04-07 14:08:03 +0200

    phone.py ändringar

---
+++ LP4/Lab3/phone/phone.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import qrcode
+
+
+
+# HÄMTA TIDEN
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+
+#GENERERA QR-KOD
+
+img = qrcode.make(current_time)
+
+img.save("sample.png")
