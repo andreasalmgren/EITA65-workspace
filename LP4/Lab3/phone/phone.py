3657d2cc5140579bfcbf600d382d77084aa93265
IndigoWilke <filip.wilke@hotmail.se>
2022-04-07 14:55:16 +0200

    fixade något med phone

--- LP4/Lab3/phone/phone.py
+++ LP4/Lab3/phone/phone.py
@@ -2,7 +2,6 @@ from datetime import datetime
 import qrcode
 
 
-
 # HÄMTA TIDEN
 now = datetime.now()
 
--- LP4/filip.py
+++ LP4/filip.py
@@ -1,4 +1,22 @@
+import rsa
 import cv2
+
+publicKey, privateKey = rsa.newkeys(1024)
+
+message = "hello geeks"
+
+encMessage = rsa.encrypt(message.encode(),
+                         publicKey)
+
+print(publicKey)
+print(privateKey)
+print("original string: ", message)
+print("encrypted string: ", encMessage)
+
+decMessage = rsa.decrypt(encMessage, privateKey).decode()
+
+print("decrypted string: ", decMessage)
+
 
 def displayBbox(im, bbox):
     if bbox is not None:
@@ -8,7 +26,7 @@ def displayBbox(im, bbox):
             cv2.line(im, tuple(bbox[0][i]), tuple(bbox[0][(i+1) % n]), (0,255,0), 3)
 
 if __name__ == '__main__':
-    img = cv2.imread('testF.png')
+    img = cv2.imread('sample.png')
     detector = cv2.QRCodeDetector()
     res, points, _ = detector.detectAndDecode(img)
     # Detected outputs.
@@ -22,3 +40,9 @@ if __name__ == '__main__':
 cv2.imshow("Image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+import qrcode
+img = qrcode.make('Some data here')
+type(img)  # qrcode.image.pil.PilImage
+img.save("some_file.png")
