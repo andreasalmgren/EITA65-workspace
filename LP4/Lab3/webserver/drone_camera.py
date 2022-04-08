c0a926885dc37b0d81a54bb0a52e28a431c16812
HugoLundberg <hugo.h.lundberg@gmail.com>
2022-04-08 14:35:05 +0200

    drone_camera.py

---
+++ LP4/Lab3/webserver/drone_camera.py
@@ -0,0 +1,9 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview()
+sleep(5)
+camera.capture('/home/pi/diggi/EITA65-workspace/LP4/bilder/test1.jpg') #directory
+camera.stop_preview()
