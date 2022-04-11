3df79f6b823f3491d7fc50231f724792ed03b778
IndigoWilke <filip.wilke@hotmail.se>
2022-04-11 16:22:37 +0200

    fick qr avläsning i filip.py att fungera

--- LP4/filip.py
+++ LP4/filip.py
@@ -1,3 +1,4 @@
+import pyzbar.pyzbar as pyzbar
 import rsa, cv2, qrcode
 from datetime import datetime
 
@@ -15,13 +16,13 @@ print("Current Time =", current_time)
 
 #GENERERA QR-KOD
 ID = 'Filips Personnummer'
-krypto = str(current_time) + ' ' + ID
-krypto = rsa.encrypt(krypto.encode(), publicKey)
+klartext = str(current_time) + ' ' + ID
+krypto = rsa.encrypt(klartext.encode(), publicKey)
+img = qrcode.make(str(krypto))
+img.save("testKrypto2.png")
+print(krypto)
 
-img = qrcode.make(krypto)
-
-img.save("testKrypto.png")
-
+print(rsa.decrypt(krypto, privateKey).decode())
 
 
 def displayBbox(im, bbox):
@@ -32,14 +33,30 @@ def displayBbox(im, bbox):
             cv2.line(im, tuple(bbox[0][i]), tuple(bbox[0][(i+1) % n]), (0,255,0), 3)
 
 if __name__ == '__main__':
-    img = cv2.imread('testKrypto.png')
+    img = cv2.imread('testKrypto2.png')
     detector = cv2.QRCodeDetector()
-    res, points, _ = detector.detectAndDecode(img)
+    print(pyzbar.decode(img))
+    res = (pyzbar.decode(img)[0].data)
+
+    isthere, points = detector.detect(img)
+    #res = detector.decode(img, points)
+    #res, points, _ = detector.detectAndDecode(img)
     # Detected outputs.
-    if len(res) > 0:
+    #print(detector.detectAndDecode(img))
+
+
+    if res:
         print('Output : ', res)
         print('Bounding Box : ', points)
         displayBbox(img, points)
+        # print((krypto))
+        # print((pyzbar.decode(img)[0].data).decode())
+        # print(bytes(pyzbar.decode(img)[0].data.decode('unicode_escape')[2:-1], encoding="raw_unicode_escape"))
+        # print(type(krypto))
+        # print(type(bytes(pyzbar.decode(img)[0].data.decode('unicode_escape')[2:-1], encoding="raw_unicode_escape")))
+        # print("test over")
+        # print(bytes(pyzbar.decode(img)[0].data.decode('unicode_escape')[2:-1], encoding="raw_unicode_escape") == krypto )
+        res = bytes(pyzbar.decode(img)[0].data.decode('unicode_escape')[2:-1], encoding="raw_unicode_escape")
         decMessage = rsa.decrypt(res, privateKey).decode()
         print("decrypted string: ", decMessage)
     else:
